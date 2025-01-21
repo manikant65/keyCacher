3692b73f731810bfee00f48abe090725fc1e01be
Manikant Kumar <manikant38110298@gmail.com>
2025-01-21 23:41:24 +0530

    Add files via upload

---
+++ cmd/coordinator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/manikant65/keyCacher/internal/coordinator"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: coordinator <port> <replication-factor>")
+		os.Exit(1)
+	}
+
+	coordinatorPort := os.Args[1]
+	replicationFactor, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Replication factor must be an integer")
+		os.Exit(1)
+	}
+
+	coordinator.InitCoordinator(coordinatorPort, replicationFactor) // Blocking call
+}
